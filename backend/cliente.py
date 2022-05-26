24cfc98ede9f48c367157c3294e48a8e691e9f1a
erick-andrade1 <erickandradesilva@hotmail.com>
2022-05-26 20:37:48 -0300

    Correções

--- backend/cliente.py
+++ backend/cliente.py
@@ -57,7 +57,7 @@ class Cliente:
     '''Clinte que se conectara com o servidor.'''
     
     def __init__(self, HOST, PORT):
-        self.host = HOST # IP do Servidor que deseja-se se conectar.. 
+        self.host = HOST # IP do Servidor que deseja se conectar.. 
         self.port = PORT # Porta usada para conectar-se ao servidor.
         self.login = Telalogin()
         self.indices = self.login.tela_login()
@@ -76,18 +76,9 @@ class Cliente:
         cliente = self._conncetion()
         data = json.dumps({"info_1": self.indices, "info_2":self.list_digitos})
         cliente.send(data.encode())
-        # Recebendo a informaçõa 
+        # Recebendo a informação
         data = cliente.recv(1000000)
         print(data.decode())
-        """
-        with open(name_file, 'wb') as file:
-            while True:
-                data = cliente.recv(1000000)
-                if not data: # Verificando se ainda existe dados para serem enviados.
-                    break
-                file.write(data)
-        print("Arquivo recebido, cheque a sua pasta!")
-        """
 
 clinte = Cliente('127.0.0.1', 4040)
 clinte.enviar_information()
